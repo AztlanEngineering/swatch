c80f86db44bf3d6e8484e678146b3e2091daf6ba
779g <aztlan@779.mx>
2020-06-12 21:08:30 +0200

    updated the colors story

--- src/stories/Colors.stories.js
+++ src/stories/Colors.stories.js
@@ -6,6 +6,14 @@ export default {
   component:ColorPalette,
 }
 
+const storyParameters = {
+ previewTabs: { 
+    'canvas': { 
+      hidden: true 
+    },
+  }
+}
+
 const THEME = [
   'primary',
   'secondary',
@@ -52,7 +60,7 @@ const themeMap = getColorMap('theme', THEME)
 const modalsMap = getColorMap('modals', MODALS)
 const wheelMap = getColorMap('wheel', WHEEL)
 
-export const defaultStyle = () => (
+export const Swatches = () => (
   <>
     <ColorPalette>
       { themeMap.map((e,i) =>
@@ -82,37 +90,39 @@ export const defaultStyle = () => (
   </>
 )
 
-const setterTester = (MAP) => (
-  MAP.map((e,i) =>
-    <div
-      className={ 'x-' + e }
-      key={i}
-    >
-      <span style={{
-        background:'var(--dark-x)',
-        color     :'var(--on-x)',
-      }}
-      >
-        Dark
-      </span>
-      <span style={{
-        background:'var(--x)',
-        color     :'var(--on-x)',
-      }}
-      >
-        Base
-      </span>
-      <span style={{
-        background:'var(--light-x)',
-        color     :'var(--on-x)',
-      }}
-      >
-        Light
-      </span>
-    </div>
-  ))
 
-export const Setters = () => (
+export const Setters = () => {
+  const setterTester = (MAP) => (
+    MAP.map((e,i) =>
+      <div
+        className={ 'x-' + e }
+        key={i}
+      >
+        <span style={{
+          background:'var(--dark-x)',
+          color     :'var(--on-x)',
+        }}
+        >
+          Dark
+        </span>
+        <span style={{
+          background:'var(--x)',
+          color     :'var(--on-x)',
+        }}
+        >
+          Base
+        </span>
+        <span style={{
+          background:'var(--light-x)',
+          color     :'var(--on-x)',
+        }}
+        >
+          Light
+        </span>
+      </div>
+    ))
+
+  return (
   <>
     <p>Theme</p>
     { setterTester(THEME) }
@@ -121,50 +131,56 @@ export const Setters = () => (
     <p>Modals</p>
     { setterTester(MODALS) }
   </>
-)
+) }
 
-const getterTester = (MAP) => (
-  MAP.map((e,i) =>
-    <>
-      <div className={ 'x-' + e }>
-        <span className={ 'b-dark-x c-on-x' }>Back Dark</span>
-        <span className={ 'b-x c-on-x' }>Back Base</span>
-        <span className={ 'b-light-x c-on-x' }>Back Light</span>
-      </div>
-      <br/>
-      <div className={ 'y-' + e }>
-        <span
-          className={ 'd-dark-y c-dark-y' }
-          style={{ borderWidth: '10px', borderStyle: 'solid' }}
-        >
-          Border dark
-        </span>
-        <span
-          className={ 'd-y c-y' }
-          style={{ borderWidth: '10px', borderStyle: 'solid' }}
-        >
-          Border base
-        </span>
-        <span
-          className={ 'd-light-y c-light-y' }
-          style={{ borderWidth: '10px', borderStyle: 'solid' }}
-        >
-          Border light
-        </span>
-      </div>
-      <br/>
-      <div className={ 'z-' + e }>
-        <span className='c-dark-z'>Color Dark</span>
-        <span className='c-z'>Color Base</span>
-        <span className='c-light-z'>Color Light</span>
-      </div>
-    </>
-  ))
+export const Getters = () => {
 
-export const Getters = () => (
+  const getterTester = (MAP) => (
+    MAP.map((e,i) =>
+      <>
+        <div className={ 'x-' + e }>
+          <span className={ 'b-dark-x c-on-x' }>Back Dark</span>
+          <span className={ 'b-x c-on-x' }>Back Base</span>
+          <span className={ 'b-light-x c-on-x' }>Back Light</span>
+        </div>
+        <br/>
+        <div className={ 'y-' + e }>
+          <span
+            className={ 'd-dark-y c-dark-y' }
+            style={{ borderWidth: '10px', borderStyle: 'solid' }}
+          >
+            Border dark
+          </span>
+          <span
+            className={ 'd-y c-y' }
+            style={{ borderWidth: '10px', borderStyle: 'solid' }}
+          >
+            Border base
+          </span>
+          <span
+            className={ 'd-light-y c-light-y' }
+            style={{ borderWidth: '10px', borderStyle: 'solid' }}
+          >
+            Border light
+          </span>
+        </div>
+        <br/>
+        <div className={ 'z-' + e }>
+          <span className='c-dark-z'>Color Dark</span>
+          <span className='c-z'>Color Base</span>
+          <span className='c-light-z'>Color Light</span>
+        </div>
+      </>
+    )) 
+
+  return(
   <>
     { getterTester(THEME) }
     { getterTester(WHEEL) }
     { getterTester(MODALS) }
   </>
-)
+)}
+
+Swatches.parameters = storyParameters
+Setters.parameters = storyParameters
+Getters.parameters = storyParameters
