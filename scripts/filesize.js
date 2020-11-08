b81e2c22d774defdc022eab9d304c721010726bb
779g <adrian.villa@779.mx>
2020-11-08 14:09:56 +0000

    Removed unused z setters/getters

---
+++ scripts/filesize.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+let fs = require('fs')
+let filesize = require('filesize')
+let targetFilename = process.argv[2]
+let stats = fs.statSync(`./${targetFilename}`)
+console.log(process.argv[2], filesize(stats.size), stats.size)
