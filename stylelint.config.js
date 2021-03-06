febc75b2cef860a564494c2dc9c772ffdc90a735
779g <adrian.villa@779.mx>
2021-03-06 19:56:37 +0000

    Linting fixes

--- stylelint.config.js
+++ stylelint.config.js
@@ -10,15 +10,29 @@ module.exports = {
       {
         ignoreAtRules: [
         // additional scss at-rules:
-          "content", "each", "else", "error", "extend", "for", "function", "if", "include", "mixin", "return", "use"
+          "content", "each", "else", "error", "extend", "for", "function", "if", "include", "mixin", "return", "use", "forward"
         ],
       }
     ],
     "at-rule-empty-line-before": ["always", {
       "except": ["after-same-name", "inside-block"],
         "ignoreAtRules": ["include", "mixin", "function"]
-    }]
+    }],
+    "order/order": [
+      [
+        "custom-properties",
+        "at-variables",
+        "dollar-variables",
+        "less-mixins",
+        /*
+        {
+          "type": "at-rule",
+          "name": "include"
+        },*/
+        "declarations",
+        "rules",
+      ],
+      { unspecified: "ignore" },
+    ],
   }
 }
-
-
